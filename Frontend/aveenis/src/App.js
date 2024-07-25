84f80246d08a399b93e3c6577c580c89a9f9f45c
sid <46699142+sbanerjee13@users.noreply.github.com>
2024-07-24 17:30:42 -0700

    Basic frontend display of post titles, accessed via Supabase

--- Frontend/aveenis/src/App.js
+++ Frontend/aveenis/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const PROJECT_URL = process.env.REACT_APP_PROJECT_URL;
+const ANON_KEY = process.env.REACT_APP_ANON_KEY;
+
+const supabase = createClient(PROJECT_URL, ANON_KEY);
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  async function getPosts() {
+    const { data } = await supabase.from("posts").select();
+    setPosts(data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome. To the Aveenis.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
       </header>
     </div>
   );
