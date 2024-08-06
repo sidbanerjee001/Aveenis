2f632805cc97da43fede3d3b041ebcbe7e077d90
sidbanerjee001 <sidbanerjee@berkeley.edu>
2024-08-06 16:44:21 -0700

    Frontend overhaul with Tailwind and HeadlessUI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,106 @@
-import Navbar from "./Components/Navbar"
-import Hero from "./Components/Hero"
+'use client'
 
-const App = () => {
+import { useState } from 'react'
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'About Us', href: '#' },
+  { name: 'Contact', href: '#' }
+]
+
+export default function Landing() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
   return (
-    <>
-      <Navbar/>
-      <Hero/>
-    </>
+    <div className="bg-white">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Aveenis</span>
+              <p>Aveenis</p>
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="transition ease-in-out text-sm font-semibold leading-6 text-black hover:text-green">
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </nav>
+        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+          <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-black/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Aveenis</span>
+                <p>Aveenis</p>
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="transition ease-in-out -mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-black hover:bg-green-hover hover:text-white"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </header>
+
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">
+              Exploring the Excellence of <span className="text-green">Aveenis</span>
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dictum, sapien sit amet pharetra pulvinar, 
+            ipsum elit pretium lacus, id tincidunt sem urna ac ipsum. Quisque urna orci, sollicitudin in nisl nec, 
+            commodo vehicula magna. 
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="transition ease-in-out rounded-md bg-green px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-green-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green"
+              >
+                Get Started
+              </a>
+              <a href="#" className="transition ease-in-out text-sm font-semibold leading-6 text-black hover:text-green-hover">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default App
