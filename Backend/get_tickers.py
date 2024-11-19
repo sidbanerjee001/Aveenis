2f8d394bfbb1e4fcc10c36e813abb35d0482c307
David Jo <tkdave44@gmail.com>
2024-11-18 17:55:40 -0800

    Wrote all tickers into tickers.txt

--- Backend/get_tickers.py
+++ Backend/get_tickers.py
@@ -1,7 +1,7 @@
-with open ('stocks.txt', 'r') as file:
+with open('/Users/hanj/Desktop/Aveenis/Backend/stocks.txt', 'r') as file:
     lines = file.readlines()
-tickers = [line.split('\t')[1] for line in lines]
 
-with open ('tickers.txt', 'w') as write_file:
+tickers = [line.split('\t')[1] for line in lines if len(line.split('\t')) > 1]
+
+with open('/Users/hanj/Desktop/Aveenis/Backend/tickers.txt', 'w') as write_file:
     write_file.write('\n'.join(tickers))
-    
