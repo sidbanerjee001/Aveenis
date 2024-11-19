0dc8ca97967b0b882fe0ed9fbc38fbd91532333a
Sid Banerjee <sidbanerjee@berkeley.edu>
2024-11-19 15:13:36 -0800

    Error handling

--- Backend/Exec.py
+++ Backend/Exec.py
@@ -12,7 +12,7 @@ def run():
     map2 = {
         "t1": [1,2,3,4],
         "t2": [1,1,1,1],
-        "t3": [1,2,3,4],
+        "t3": [1,2,3],
         "t4": [1,2,3,4],
         "t6": [1,2,3,4],
     } # replace with call to Stocktwits scrape
@@ -26,11 +26,11 @@ def run():
     for e in flattened:
         ticker = e[0]
         values = e[1]
-        set.add(len(values))
+        lengths.add(len(values))
         combined[ticker] = [sum(x) for x in zip(combined.get(ticker, [0 for _ in range(len(values))]), values)]
     
     if len(lengths) > 1:
-        print("Malformed data; lengths of size", lengths, "exist in scraped data.")
+        print("Malformed data; lengths of sizes", lengths, "exist in scraped data.")
         return None
 
     return dict(map(lambda e: (e[0], calculate_function(e[1])), combined.items()))
