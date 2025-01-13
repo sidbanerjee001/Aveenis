dbb04ace4af08cff42ec580017031a952318ff72
Shamik Karkhanis <shamikkark@gmail.com>
2025-01-13 08:14:52 -0800

    Update database.py

--- Backend/modules/database.py
+++ Backend/modules/database.py
@@ -206,7 +206,7 @@ class Database:
 
         return (
             Data(
-                _table_name,
+                "ticker",
                 response[0],
             )
             if response
@@ -227,6 +227,6 @@ class Database:
         # _data.set_value("updated_at", str(datetime.datetime.now(datetime.UTC)))
 
         # Update the data in the database
-        self.__client.table(_data.get_value("type")).upsert(
+        self.__client.table("final_db").upsert(
             {"stock_ticker": _stock_ticker, "data": str(_data)}
         ).execute()
