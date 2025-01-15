5a2ee04c831d89f7a9b73b716b3325e6b4a1af30
sidbanerjee001 <sidbanerjee@berkeley.edu>
2025-01-14 22:34:16 -0800

    Updates properly with 24h refresh

--- Backend/Exec.py
+++ Backend/Exec.py
@@ -30,13 +30,19 @@ def run():
     # return dict(map(lambda e: (e[0], calculate_function(e[1])), combined.items()))
 
     db = Database()
+    
+    # calculate IV with call to wiv.py, requires yfinance and numpy
 
-    # save the dictionary "combined" into the supabase (append metrics)
+    # save the dictionary "combined" into the supabase (append metrics) + WIV
 
     for ticker, score in combined.items():
         data = db.get_data("final_db", ticker)
         if data:
+            if len(data.get_value("data_today") == 24):
+                data.pop_value("data_today", 0)
             data.append_value("data_today", score)
+            # Add append_value for wiv data
             db.upsert_data(ticker, data)
+
 
     return 0
