05b46372f3b63562b3a1c63a3841d58d51c46a0b
sidbanerjee001 <156862741+sidbanerjee001@users.noreply.github.com>
2025-01-16 13:14:17 -0800

    Delete Backend/web-ui directory

--- Backend/web-ui/src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <>
-        <footer className="absolute mb-5 text-center bottom-0 w-full text-xs">Aveenis © 2024, All Rights Reserved</footer>
-        </>
-    )
-}
