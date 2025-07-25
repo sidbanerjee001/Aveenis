f90a4b021e31c1562d8a5963ae9bdef1d19fe8b2
Jovan Radovic <jradovic@ucdavis.edu>
2025-07-25 12:59:15 -0700

    Only score Accel

--- Backend/Exec.py
+++ Backend/Exec.py
@@ -41,6 +41,8 @@ def update_ticker(ticker, datas, supabase_client, database_name="full_data_with_
         likes_total_full[0] = datas["total_likes"]
         iv_total_full = [0] * 30
         iv_total_full[0] = datas["wiv"]
+        scores = [0] * 30
+        scores[0] = datas['daily_score']
 
         # Prepare the data to insert, including the ticker
         data_with_ticker = {
@@ -49,13 +51,11 @@ def update_ticker(ticker, datas, supabase_client, database_name="full_data_with_
             "likes_hourly": json.dumps(datas["likes"]),
             "mentions_daily": json.dumps(mentions_total_full),
             "likes_daily": json.dumps(likes_total_full),
-            "mentions_hourly_acceleration": json.dumps(datas["hourly_mentions_acceleration"]),
-            "likes_hourly_acceleration": json.dumps(datas["hourly_likes_acceleration"]),
             "stock_price": json.dumps(datas["stock_price"]),
             "market_cap": json.dumps(datas["market_cap"]),
-            "hourly_stock_price_acceleration": json.dumps(datas["hourly_stock_price_acceleration"]),
-            "hourly_market_cap_acceleration": json.dumps(datas["hourly_market_cap_acceleration"]),
             "wiv": json.dumps(iv_total_full),
+            "daily_score": json.dumps(datas['daily_score']),
+            "daily_scores_acceleration": json.dumps([0] * 30)
         }
         supabase_client.table(database_name).insert(data_with_ticker).execute()
         print(f"Inserted new data for {ticker}")
@@ -65,6 +65,8 @@ def update_ticker(ticker, datas, supabase_client, database_name="full_data_with_
         old_mentions_total = json.loads(response.data[0].get("mentions_daily"))
         old_likes_total = json.loads(response.data[0].get("likes_daily"))
         old_wiv = json.loads(response.data[0].get("wiv"))
+        old_daily_scores = json.loads(response.data[0].get("daily_score"))
+        old_daily_scores_accel = json.loads(response.data[0].get("daily_scores_acceleration"))
         
         old_mentions_total.pop()
         old_mentions_total.insert(0, datas["total_mentions"])
@@ -75,19 +77,23 @@ def update_ticker(ticker, datas, supabase_client, database_name="full_data_with_
         old_wiv.pop()
         old_wiv.insert(0, datas["wiv"])
         
+        old_daily_scores.pop()
+        old_daily_scores.insert(0, datas['daily_score'])
+        
+        # Calculate score acceleration
+        old_daily_scores_accel = calculate_accels(old_daily_scores)
+        
         # Update the existing record with new data
         supabase_client.table(database_name).update({
             "mentions_hourly": json.dumps(datas["hours"]),
             "likes_hourly": json.dumps(datas["likes"]),
             "mentions_daily": json.dumps(old_mentions_total),
             "likes_daily": json.dumps(old_likes_total),
-            "mentions_hourly_acceleration": json.dumps(datas["hourly_mentions_acceleration"]),
-            "likes_hourly_acceleration": json.dumps(datas["hourly_likes_acceleration"]),
             "stock_price": json.dumps(datas["stock_price"]),
             "market_cap": json.dumps(datas["market_cap"]),
-            "hourly_stock_price_acceleration": json.dumps(datas["hourly_stock_price_acceleration"]),
-            "hourly_market_cap_acceleration": json.dumps(datas["hourly_market_cap_acceleration"]),
             "wiv": json.dumps(old_wiv),
+            "daily_score": json.dumps(datas['daily_score']),
+            "daily_scores_acceleration": json.dumps(old_daily_scores_accel)
         }).eq("ticker", ticker).execute()
 
 def run_stocktwits_scrape():
@@ -122,18 +128,18 @@ def run_stocktwits_scrape():
 def run():
     # Perform scraping
     # Load results
+    
+    # Debug:
+    # with open("ex_reddit.txt", 'r') as file:
+    #     text = file.read()
+    # map1 = eval(text)
+    
     map1 = run_reddit_scrape()
     map1 = { key.replace('$', ''): value for key, value in map1.items() }
-    
     # run_stocktwits_scrape()
     map2 = load_joblib("supervised_results.joblib")
 
-    # Debug: check data types
-    # for ticker, values in map1.items():
-    #     print(f"map1 - {ticker}: {type(values)} = {values}")
-    # for ticker, values in map2.items():
-    #     print(f"map2 - {ticker}: {type(values)} = {values}")
-
+    # Combine results
     flattened = [(ticker, (int(values[0]), int(values[1]))) for ticker, values in map1.items()]
     flattened.extend([(ticker, (int(values['total_likes']), int(values["total_mentions"]))) for ticker, values in map2.items() if isinstance(values, dict)])
     combined = {}
@@ -147,9 +153,13 @@ def run():
     
     # only adds to supabase if ticker also in map2 (map2 will contain all NYSE tickers)
     for ticker_data, data in map2.items():
+        # Get combined total likes/mentions
         data['total_likes'] = combined[ticker_data][1]
         data['total_mentions'] = combined[ticker_data][0]
 
+        # Get score
+        data['daily_score'] = calculate_function([data['total_mentions'], data['total_likes']])
+        
         # Add IV_sum, daily value
         try:
             data['wiv'] = int(wiv.calculate_iv_sum(ticker_data))
@@ -163,18 +173,6 @@ def run():
         # Adding market cap, hourly
         data['market_cap'] = wiv.get_marketcap_last_day(ticker_data)
 
-        # Adding accelerations for likes, mentions, stock_price
-        # check calculate_accels; not sure if im seeing the vision
-        mentions_accel = calculate_accels(data['hours'])
-        likes_accel = calculate_accels(data['likes'])
-        stock_price_accel = calculate_accels(data['stock_price'])
-        market_cap_accel = calculate_accels(data['market_cap'])
-
-        data['hourly_mentions_acceleration'] = mentions_accel
-        data['hourly_likes_acceleration'] = likes_accel
-        data['hourly_stock_price_acceleration'] = stock_price_accel
-        data['hourly_market_cap_acceleration'] = market_cap_accel
-
     # Uploading to Supabase
     supabase_client = supabase.create_client(url, key)
 
@@ -183,29 +181,7 @@ def run():
         update_ticker(ticker, datas, supabase_client)
         print(f"Updated {ticker} in Supabase")
         
-    # return dict(map(lambda e: (e[0], calculate_function(e[1])), combined.items()))
-
-    # db = Database()
-    
-    # calculate IV with call to wiv.py, requires yfinance and numpy
-
-    # save the dictionary "combined" into the supabase (append metrics) + WIV
-
-    
-    # for ticker, score in combined.items():
-    #     data = db.get_data("full_data", ticker)
-    #     if data:
-    #         if len(data.get_value("data_today")) == 24:
-    #             data.pop_value("data_today", 0)
-    #         data.append_value("data_today", score)
-    #         # Add append_value for wiv data
-    #         db.upsert_data(ticker, data)
-    #     else: 
-    #         data = Data(_type="ticker", _stock_ticker=ticker)
-    #         data.set_value("data_today", [score])
-    #         db.upsert_data(ticker, data)
-
-    # return 0
+    return 0
 
 if __name__ == "__main__":
     run()
